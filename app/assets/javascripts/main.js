fc880c1d1faf79c7776becc1eafa8e7af5560a8b
Adam McKnight <adammcknight@me.com>
2020-01-15 22:44:03 -0500

    First Commit of Project

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,1 +1,7 @@
-// Replace With Your Content
+console.log("JS Running...");
+
+// let menu = document.querySelectorAll("nav");
+
+function iconClicked(){
+    document.getElementById("nav").style.display="block";
+}
